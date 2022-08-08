85a514be15af4e21ba3864a56648487399de337a
xuyang <792865726@qq.com>
2022-08-08 22:12:16 +0800

    chore: 添加 html-webpack-plugin 插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
+  entry: {
+    bundle: './src/index.js',
   },
+  output: {
+    filename: '[name]-[hash:7].js',
+    path: path.join(__dirname, '/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
 }
